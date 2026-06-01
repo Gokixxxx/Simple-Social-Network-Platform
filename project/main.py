9335a06e77c69954f6595f29996a24624a01e95f
gokix <ge1999tian@gmail.com>
2026-06-01 14:54:16 +0800

    fix :: bug

--- project/main.py
+++ project/main.py
@@ -50,7 +50,7 @@ class mySocialNetwork:
     def get_input(self, prompt: str, required: bool = True) -> str:
         """get user input"""
         while True:
-            value = input(f"> {prompt}: ").strip()
+            value = input(f"> {prompt} ").strip()
             if value.lower() == 'q':
                 return None
             if not required or value:
